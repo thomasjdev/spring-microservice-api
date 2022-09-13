ad4342d5b3754e8e930643f28f7869851154773e
Thomas Anderson <thomas@heythomasj.com>
2022-09-12 21:40:49 -0700

    initial commit

---
+++ src/main/java/com/thomasjdev/springmicroserviceapi/SpringMicroserviceApiApplication.java
@@ -0,0 +1,13 @@
+package com.thomasjdev.springmicroserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroserviceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thomasjdev/springmicroserviceapi/SpringMicroserviceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomasjdev.springmicroserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
