a2c893ddef74bbf5c0390935bb8daf7f4956bf1f
Thomas Anderson <thomas@heythomasj.com>
2022-09-12 22:41:47 -0700

    add simple metrics exposure to actuator

--- src/main/java/com/thomasjdev/springmicroserviceapi/SpringMicroserviceApiApplication.java
+++ src/main/java/com/thomasjdev/springmicroserviceapi/SpringMicroserviceApiApplication.java
@@ -1,13 +1,48 @@
 package com.thomasjdev.springmicroserviceapi;
 
+import io.micrometer.core.annotation.Timed;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+import java.util.Set;
+
+@Slf4j
 @SpringBootApplication
 public class SpringMicroserviceApiApplication {
 
     public static void main(String[] args) {
+        log.info("Starting Spring Server");
         SpringApplication.run(SpringMicroserviceApiApplication.class, args);
     }
 
+}
+
+@RestController
+class AvailabilityController {
+    private boolean validate(String console) {
+        return StringUtils.hasText(console) &&
+                Set.of("ps5", "ps4", "switch", "xbox").contains(console);
+    }
+
+    @GetMapping("/availability/{console}")
+    Map<String, Object> getAvailability(@PathVariable String console) {
+        return Map.of("console", console, "availability", checkAvailabliity(console));
+    }
+
+    private boolean checkAvailabliity(String console) {
+        Assert.state(validate(console), () -> "the console specified, " + console + ", is not valid.");
+        return switch (console) {
+            case "ps5" -> throw new RuntimeException("Service exception");
+            case "xbox" -> true;
+            default -> false;
+        };
+    }
 }
